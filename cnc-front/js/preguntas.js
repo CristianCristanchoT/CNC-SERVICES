54a0fba5962ae0389dd5096499dad19195078c65
santii <santiago_vp_25@hotmail.com>
2021-11-10 16:33:46 -0500

    Se añadio ultima version front

--- cnc-front/js/preguntas.js
+++ cnc-front/js/preguntas.js
@@ -22,11 +22,29 @@ async function get_response(input, url) {
     }
 }
 
+async function get_document(input, url) {
+    try {
+        const response = await fetch(url,
+            {
+                method: 'POST',
+                body: JSON.stringify(input), // data can be `string` or {object}!
+                headers: {
+                    'Content-Type': 'application/json',
+                    'responseType': 'arraybuffer'
+                }
+            });
+        const result = await response.blob();
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 async function main() {
     grupos = [];
     var root_url = "http://209.50.62.200:8080/obterner_preguntas";
     var parameters = sessionStorage.getItem('parameters');
-    console.log(parameters);
+    // estructura parameters
     // var parameters = {
     //     "sector": "EMPAQUES",
     //     "segmento": "CLIENTES DESECHABLES",
@@ -43,7 +61,7 @@ async function main() {
             grupos.push(element.Categorias);
             // crear div contenedor (card)
             var newDiv = document.createElement("div");
-            newDiv.className = "card boxshadow p-3 mb-3 bg-white rounded";
+            newDiv.className = "card boxshadow p-3 mb-3 bg-white rounded scrollbar";
             newDiv.setAttribute('id', `div_${element.Categorias}`);
             newDiv.style.height = "300px";
             newDiv.style.overflowY = "scroll";
@@ -65,7 +83,15 @@ crearBody = (data, grupos) => {
         let cadenaHTML = "";
         cadenaHTML += `<div class="card-body">`;
         cadenaHTML += `
-                    <h3><span class="verticalsector-dos"></span>&nbsp;&nbsp;&nbsp;${dataDiv[0].Categorias}</h3>`
+                    <div class="row d-flex">
+                        <div class="col">
+                            <h3><span class="verticalsector-dos"></span>&nbsp;&nbsp;&nbsp;${dataDiv[0].Categorias}</h3>
+                        </div>
+                        <div class="col">
+                            <button class="btn btn-primary btn-lg" style="float: right;">Nueva pregunta</button>
+                        </div>
+                    </div>
+                    `;
         dataDiv.forEach((element, index) => {
             cadenaHTML += `
                     <div class="form-check">
@@ -88,21 +114,35 @@ crearBody = (data, grupos) => {
 
 main();
 
-enviar = () => {
-    console.log(arrayCheckbox);
-
+enviar = async () => {
     let arrayJSON = dataGlobal.filter(element => {
         if (arrayCheckbox.includes(element._id.$oid)) {
             return element
         }
     });
+    // debugger;
+    var parameters = JSON.parse(sessionStorage.getItem('parameters'));
 
     let dataRequest = {
         "datos_busqueda": parameters,
         "preguntas_seleccionadas": arrayJSON
     }
-    var parameters = sessionStorage.getItem('parameters');
-    console.log(dataRequest);
+
+    // let test = JSON.parse(dataRequest);
+
+    // console.log(test);
+
+    let headers = {
+        'Content-Type': 'application/json'
+    }
+
+    var root_url = "http://209.50.62.200:8080/procesar_preguntas";
+    // var data = await get_document(JSON.parse(dataRequest), root_url);
+    var data = await get_document(dataRequest, root_url);
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(data);
+    link.download = "myFileName.docx";
+    link.click();
 }
 
 // CHECKBOX
